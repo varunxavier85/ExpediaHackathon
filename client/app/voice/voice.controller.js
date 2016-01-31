078903bb7ab4facde0d488b57d4703438b59a83a
harjas <harjassinghglobal@gmail.com>
2016-01-30 21:35:33 -0800

    client

---
+++ client/app/voice/voice.controller.js
@@ -0,0 +1,8 @@
+angular.module('makemetravel').controller('VoiceController', VoiceController);
+
+VoiceController.$inject = [];
+
+function VoiceController() {
+  var _this = this;
+  _this.helloworld = 'Hello, Voice!';
+}
